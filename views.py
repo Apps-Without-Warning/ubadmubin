b7f20406419de30d9584d8442bf69452a8f1b264
Alex Burka <web@alexburka.com>
2020-12-20 02:40:02 +0000

    add meeting settings checkboxes

--- views.py
+++ views.py
@@ -39,33 +39,24 @@ def meeting(request, meeting_id, occurrence_id=None):
 
     token = zoom.gen_token()
 
+    editable_settings = [
+        ('waiting_room', 'Use waiting room', 'boolean'),
+        ('request_permission_to_unmute_participants', 'Request permission to unmute participants', 'boolean'),
+    ]
+
     try:
         meeting, _ = zoom.get_meeting(token, meeting_id)
     except zoom.Error as e:
         data['response_code'] = e.code
     else:
         if request.method == 'POST':
-            assert 'ocurrences' not in meeting, 'Editing recurring meetings is not supported'
-
-            updates = {}
             post = {k: urllib.parse.unquote(v) for k, v in request.POST.items()}
+            if post['action'] == 'settings':
+                updates = {'settings': {}}
 
-            # use a whitelist of properties that can be set
-            for prop in ('topic', 'password', 'agenda'):
-                updates[prop] = post[prop]
-
-            # start_time is split into three properties
-            updates['start_time'] = '%(start_time-date)sT%(start_time-time)s:00' % post
-            updates['timezone'] = post['timezone']
-
-            # duration is passed as end_time
-            fmt = '%Y-%m-%dT%H:%M:%S'
-            start = datetime.strptime(updates['start_time'], fmt)
-            end = datetime.strptime('%(start_time-date)sT%(end_time)s:00' % post, fmt)
-            updates['duration'] = round((end - start).seconds / 60)
-
-            if post['action'] == 'update':
-                # edit the current meeting, then show it again
+                for setting in editable_settings:
+                    if setting[2] == 'boolean':
+                        updates['settings'][setting[0]] = setting[0] in post
 
                 event = Event.objects.create(user=request.user, event='UP', meeting_id=meeting_id, data=json.dumps(updates))
                 event.save()
@@ -78,24 +69,57 @@ def meeting(request, meeting_id, occurrence_id=None):
                     error = urllib.request.quote(json.dumps(e.data))
 
                 return HttpResponseRedirect('?response_code=%d&error=%s' % (code, error)) # redirect so that refreshing doesn't POST again
+            else:
+                assert 'ocurrences' not in meeting, 'Editing recurring meetings is not supported'
 
-            elif post['action'] == 'create':
-                # create a new meeting, then show that one
+                updates = {}
 
-                for k, v in updates.items():
-                    meeting[k] = v
+                # use a whitelist of properties that can be set
+                for prop in ('topic', 'password', 'agenda'):
+                    updates[prop] = post[prop]
 
-                event = Event.objects.create(user=request.user, event='CR', meeting_id=meeting_id, data=json.dumps(meeting))
-                event.save()
+                # start_time is split into three properties
+                updates['start_time'] = '%(start_time-date)sT%(start_time-time)s:00' % post
+                updates['timezone'] = post['timezone']
 
-                try:
-                    meeting, code = zoom.create_meeting(token, meeting)
-                except zoom.Error as e:
-                    code = e.code
-                    error = urllib.request.quote(json.dumps(e.data))
+                # duration is passed as end_time
+                fmt = '%Y-%m-%dT%H:%M:%S'
+                start = datetime.strptime(updates['start_time'], fmt)
+                end = datetime.strptime('%(start_time-date)sT%(end_time)s:00' % post, fmt)
+                updates['duration'] = round((end - start).seconds / 60)
+
+                if post['action'] == 'update':
+                    # edit the current meeting, then show it again
+
+                    event = Event.objects.create(user=request.user, event='UP', meeting_id=meeting_id, data=json.dumps(updates))
+                    event.save()
+
+                    try:
+                        _, code = zoom.update_meeting(token, meeting_id, updates)
+                        error = ''
+                    except zoom.Error as e:
+                        code = e.code
+                        error = urllib.request.quote(json.dumps(e.data))
+
                     return HttpResponseRedirect('?response_code=%d&error=%s' % (code, error)) # redirect so that refreshing doesn't POST again
 
-                return redirect('meeting', meeting_id=meeting['id'])
+                elif post['action'] == 'create':
+                    # create a new meeting, then show that one
+
+                    for k, v in updates.items():
+                        meeting[k] = v
+
+                    event = Event.objects.create(user=request.user, event='CR', meeting_id=meeting_id, data=json.dumps(meeting))
+                    event.save()
+
+                    try:
+                        meeting, code = zoom.create_meeting(token, meeting)
+                    except zoom.Error as e:
+                        code = e.code
+                        error = urllib.request.quote(json.dumps(e.data))
+                        return HttpResponseRedirect('?response_code=%d&error=%s' % (code, error)) # redirect so that refreshing doesn't POST again
+
+                    return redirect('meeting', meeting_id=meeting['id'])
 
         # this is now a GET request, show the meeting info
 
@@ -114,6 +138,7 @@ def meeting(request, meeting_id, occurrence_id=None):
 
         data['meeting'] = meeting
         data['registrants'] = registrants
+        data['editable_settings'] = editable_settings
 
         try:
             # if it's a past meeting, get attendees (FIXME won't work for recurring meetings)
